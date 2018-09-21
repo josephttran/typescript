18be8d447ec5d703eabc8108a5ef03666f37d669
JosephTTran <42635012+JosephTTran@users.noreply.github.com>
2018-09-20 23:07:04 -0700

    Classes

---
+++ classes.ts
@@ -0,0 +1,27 @@
+class Person {
+  protected name: string;
+  protected age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+class Employee extends Person {
+  private department: string;
+
+  constructor(name: string, age: number, department: string) {
+    super(name, age);
+    this.department = department;
+  }
+
+  public getInfo() {
+    return `${this.name}, ${this.age}, ${this.department}`
+  }
+}
+
+let Bob = new Employee("Bob", 22, "remote");
+
+console.log(Bob.getInfo());
+
